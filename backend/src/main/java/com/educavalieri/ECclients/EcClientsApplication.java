32a1c5088ac5d367e829c0543defbb53bd33173b
educavalieri <eduardocavalieri@hotmail.com>
2022-01-28 11:40:58 -0300

    project started, h2 configuration

---
+++ backend/src/main/java/com/educavalieri/ECclients/EcClientsApplication.java
@@ -0,0 +1,13 @@
+package com.educavalieri.ECclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcClientsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/educavalieri/ECclients/EcClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educavalieri.ECclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
